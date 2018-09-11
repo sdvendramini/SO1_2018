809221c833877a879dd99563201c76eca895a001
Alvaro <a.alaniz18@hotmail.com>
2018-09-11 18:57:20 -0300

    Andando parte C-1

--- ksamp/main.c
+++ ksamp/main.c
@@ -12,14 +12,14 @@ int main( int argc, char *argv[])
 	printUpTime();
 	printFileSystems();
 	
-	const char optionS[2] = "-s";	
+	const char *optionS = "-s";	
 
-	if(argc > 1 ){		
+	if(argc > 1 ){
 		if(strcmp(argv[1], optionS)==0){
-			//reemplazar este printf por el llamado a las funciones de la parte C
-			printf("%s\n","Opcion ingresada : -s ");
+			cpuTime();
 		}
 	}
+	
 	return 0;
 	
 }
