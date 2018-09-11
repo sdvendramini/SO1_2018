4f2094a2b60884a01f598f0c5b637e2404d43fa9
sdvendramini <sdvendramini@gmail.com>
2018-09-11 18:34:21 -0300

    Se agrega logica para argumento -s

--- ksamp/main.c
+++ ksamp/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 #include "functions.h"
 
-int main()
+int main( int argc, char *argv[])
 {	
+	
 	printHostname();
 	printDate();
 	printCpuInfo();
@@ -10,8 +12,14 @@ int main()
 	printUpTime();
 	printFileSystems();
 	
+	const char optionS[2] = "-s";	
+
+	if(argc > 1 ){		
+		if(strcmp(argv[1], optionS)==0){
+			//reemplazar este printf por el llamado a las funciones de la parte C
+			printf("%s\n","Opcion ingresada : -s ");
+		}
+	}
 	return 0;
+	
 }
-
-
-
